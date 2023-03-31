15fd780580d662dcf715c90159ed657221464ce0
LinhNguyennnn <linhna.2049@gmail.com>
2023-04-01 00:54:42 +0700

    fix ip address

--- src/libs/utils/axios.ts
+++ src/libs/utils/axios.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  // IP address
-  baseURL: 'http://localhost:8800/api',
-  // baseURL: 'http://192.168.0.6:8800/api',
+  baseURL: 'https://ghpvina.com/api',
   timeout: 100000,
 });
